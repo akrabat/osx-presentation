2e560804fceda05383a874efdfebd3416b7a090c
Renaud Blanch <blanch@imag.fr>
2022-11-16 14:32:28 +0100

    preparing a 3.2 beta

--- presentation.py
+++ presentation.py
@@ -29,7 +29,7 @@ from collections import defaultdict
 
 NAME = "Présentation"
 ID = "fr.imag.iihm.blanch.osx-presentation"
-MAJOR, MINOR, PATCH, BETA = 3, 1, 1, ''
+MAJOR, MINOR, PATCH, BETA = 3, 2, 0, 'b'
 VERSION = "%s.%s.%s%s" % (MAJOR, MINOR, PATCH, BETA)
 HOME = "http://iihm.imag.fr/blanch/software/osx-presentation/"
 COPYRIGHT = "Copyright © 2011-2022 Renaud Blanch"
